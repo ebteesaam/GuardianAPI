d7db22277025b90ff7c0a6fda1bd9d9829013d07
Ebtesam Harith <ebtesam@github.com>
2018-01-07 20:31:10 +0300

    after added settings

---
+++ app/src/main/java/com/example/android/guardianapi/SettingActivity.java
@@ -0,0 +1,58 @@
+package com.example.android.guardianapi;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by ebtesam on 1/7/2018 AD.
+ */
+
+public class SettingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.settings_activity);
+    }
+
+    public static class EarthquakePreferenceFragment extends PreferenceFragment implements Preference.OnPreferenceChangeListener {
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.settings_main);
+
+            Preference byOrder = findPreference(getString(R.string.settings_order_by_key));
+            bindPreferenceSummaryToValue(byOrder);
+        }
+
+        private void bindPreferenceSummaryToValue(Preference preference) {
+            preference.setOnPreferenceChangeListener(this);
+            SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(preference.getContext());
+            String preferenceString = preferences.getString(preference.getKey(), "");
+            onPreferenceChange(preference, preferenceString);
+        }
+
+
+        @Override
+        public boolean onPreferenceChange(Preference preference, Object o) {
+            String stringValue = o.toString();
+            if (o instanceof ListPreference) {
+                ListPreference listPreference = (ListPreference) preference;
+                int prefindix = listPreference.findIndexOfValue(stringValue);
+                if (prefindix >= 0) {
+                    CharSequence[] labels = listPreference.getEntries();
+                    preference.setSummary(labels[prefindix]);
+                }
+            } else {
+                preference.setSummary(stringValue);
+            }
+            return true;
+        }
+
+    }
+}
